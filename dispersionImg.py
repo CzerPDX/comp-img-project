6945b30e09ac66cad4e52b34c713a8a05515b887
czer <czer@pdx.edu>
2023-03-16 16:21:39 -0700

    added functions to separate photosites into their own channels by type and another to display them

--- dispersionImg.py
+++ dispersionImg.py
@@ -2,6 +2,9 @@
 # The DispersionImg class takes in a rawpy-compatible Raw image dispersed by a prism
 # It then gathers and manages the initial information and processes it and then attempts to undisperse the image
 
+# References: 
+# https://numpy.org/doc/stable/reference/arrays.indexing.html#basic-slicing-and-indexing
+
 import numpy as np
 import rawpy
 import cv2
@@ -19,6 +22,7 @@ class DispersionImg:
         self.processedImg = None                # Processed image from RAW image (often creates RGB) as numpy array
         self.smallerImg = None                  # Smaller image for processing easier as numpy array
         self.maxDimensionPx = maxDimensionPx    # Max in either dimension for smaller image
+        self.rawChannels = {}                   # Will hold the raw, unaligned input from each type of image sensor separated into 1/4 size numpy arrays
         self.manuallyDemosaicedRaw = None       #Image is demosaiced manually into each channel keeping raw values
 
         # Save image information
@@ -35,6 +39,30 @@ class DispersionImg:
             plt.imshow(self.smallerImg)
             plt.title(f'Smaller processed image: {self.smallerImg.shape}')
             plt.show()
+
+    def displayRawChannels(self):
+        # Display the RAW data from each photosite
+        # Add Green2 channel to upper left to match rgbg Bayer pattern of:
+        # G2 B
+        # R G1
+        plt.suptitle(f'RAW photosite by sensor type for {self.parameters["color_desc"]} Bayer pattern', fontweight = 'bold')
+        plt.subplot(2, 2, 1)
+        plt.title('Green2')
+        plt.imshow(self.channels['green2'], cmap='Greens')
+
+        plt.subplot(2, 2, 2)
+        plt.title('Blue')
+        plt.imshow(self.channels['blue'], cmap='Blues')
+
+        plt.subplot(2, 2, 3)
+        plt.title('Red')
+        plt.imshow(self.channels['red'], cmap='Reds')
+
+        plt.subplot(2, 2, 4)
+        plt.title('Green1')
+        plt.imshow(self.channels['green1'], cmap='Greens')
+
+        plt.show() 
         
     # Overwrites class data
     def resetImg(self, imgLocationUnNormed):
@@ -54,7 +82,10 @@ class DispersionImg:
                 # Save the raw image
                 self.rawImg = rawImg.raw_image.copy()
 
+
                 # Save the rawpy metadata
+                # Eventually this should use getattr() so it can accommodate other camers besides mine
+                # but I'm working with this unsafe version right now because it's safe for my camera!
                 self.parameters = {
                     'raw_type': rawImg.raw_type,
                     'black_level_per_channel': rawImg.black_level_per_channel,
@@ -65,6 +96,7 @@ class DispersionImg:
                     'daylight_whitebalance': rawImg.daylight_whitebalance,
                     'num_colors': rawImg.num_colors,
                     'raw_colors': rawImg.raw_colors,
+                    'raw_pattern': rawImg.raw_pattern,
                     'raw_image_visible': rawImg.raw_image_visible.copy(),
                     'rgb_xyz_matrix': rawImg.rgb_xyz_matrix,
                     'sizes': rawImg.sizes,
@@ -78,7 +110,8 @@ class DispersionImg:
                 # Make a smaller image to work with, if either dimension is larger than 512 pixels
                 self.smallerImg = self.__reduceProcessedImg()
 
-                self.manuallyDemosaicedRaw = self.__manualDemosaicKeepRawData()
+                # Separate the raw data into channels by photosite type (ie all Reds in one, Blues in another, etc...)
+                self.__manualDemosaicKeepRawData()
 
             if self.imgError is False:
                 print('Image data successfully Loaded!')
@@ -128,11 +161,23 @@ class DispersionImg:
         except Exception as err:
             raise(f'Error! Could not reduce size of processed image: {err}')
         
-
-    # This method demosaics the raw data into the number of channels in the bayer pattern
-    # It does not adjust the values of any of the data.
+    # Separate each photosite on the image sensor into its own channel based on the Bayer pattern
+    # This will only work for b'RGBG' bayer pattern for now. 
+    # Future work will attempt to expand this area to more Bayer patterns programmatically
+    # This is used temporarily for getting the proof of concept out
     def __manualDemosaicKeepRawData(self):
-        print(f'manualDemosaicKeepRawData started')
+
+        try:
+            self.channels = {}
+            self.channels['green2'] = self.rawImg[0::2, 0::2].copy()   # From every other row starting at row 0, choose every other pixel starting at 0
+            self.channels['blue'] = self.rawImg[0::2, 1::2].copy()     # From every other row starting at row 0, choose every other pixel starting at 1
+            self.channels['red'] = self.rawImg[1::2, 0::2].copy()      # From every other row starting at row 1, choose every other pixel starting at 0
+            self.channels['green1'] = self.rawImg[1::2, 1::2].copy()   # From every other row starting at row 1, choose every other pixel starting at 1
+            
+            print('Photosites successfully separated into their own images under "channels" object!')
+
+        except Exception as err:
+            raise Exception(f'Error! Failed to demosaic raw data:\n{err}')
 
 
 def testDispersionImg():
@@ -140,8 +185,12 @@ def testDispersionImg():
     
     dispersedImgObj = DispersionImg(imgLocation, 512)
 
-    # Print the information about the object to the console
-    dispersedImgObj.printImageInformation()
+    dispersedImgObj.displayRawChannels()
+
+    # # Print the information about the object to the console
+    # dispersedImgObj.printImageInformation()
+
+    # print(dispersedImgObj.parameters['raw_colors'].shape)
 
 
 
